cc5350670550516603da4f140065f9c38013fadd
Lauren Mellon <laumellon@gmail.com>
2019-04-15 19:40:49 -0400

    added ajax and event listeners for year buttons

--- ombd-app/app.js
+++ ombd-app/app.js
@@ -39,7 +39,116 @@ $('.previous').on('click',() => {
   $visiblePic.eq(picCount).show();
 })
 
+// create variable for t= in ajax url
+let movieName = '';
+
+// event listener for 1985 button
+$('.80sbtn').on('click',(event) => {
+  movieName = 'out+of+africa';
+
+// use AJAX to retrieve data from the omdb api
+  $.ajax({
+    url: "http://www.omdbapi.com/?i=tt3896198&apikey=c55d5c9f&t=" + movieName
+  }).then(
+    (data) => {
+      console.log(data);
+      $('.movie-info').empty();
+      let $title = $('<h3>').addClass('title').html(data.Title);
+      let $plot = $('<div>').addClass('plot').html(data.Plot);
+      let $director = $('<div>').addClass('director').html('Directed by: ' + (data.Director));
+      let $stars = $('<div>').addClass('stars').html('Cast: ' + (data.Actors));
+      $('.movie-info').append($title);
+      $('.movie-info').append($plot);
+      $('.movie-info').append($director);
+      $('.movie-info').append($stars);
+    },
+    () => {
+      console.log('bad request');
+    }
+  )
+});
 
 
+//event listener for 1996 button
+$('.90sbtn').on('click',(event) => {
+  movieName = 'The+bridges+of+madison+county';
+
+// use AJAX to retrieve data from the omdb api
+  $.ajax({
+    url: "http://www.omdbapi.com/?i=tt3896198&apikey=c55d5c9f&t=" + movieName
+  }).then(
+    (data) => {
+      console.log(data);
+      $('.movie-info').empty();
+      let $title = $('<h3>').addClass('title').html(data.Title);
+      let $plot = $('<div>').addClass('plot').html(data.Plot);
+      let $director = $('<div>').addClass('director').html('Directed by: ' + (data.Director));
+      let $stars = $('<div>').addClass('stars').html('Cast: ' + (data.Actors));
+      $('.movie-info').append($title);
+      $('.movie-info').append($plot);
+      $('.movie-info').append($director);
+      $('.movie-info').append($stars);
+    },
+    () => {
+      console.log('bad request');
+    }
+  )
+});
+
+//event listener for 2006 button
+$('.00sbtn').on('click',(event) => {
+  movieName = 'The+devil+wears+Prada';
+
+// use AJAX to retrieve data from the omdb api
+  $.ajax({
+    url: "http://www.omdbapi.com/?i=tt3896198&apikey=c55d5c9f&t=" + movieName
+  }).then(
+    (data) => {
+      console.log(data);
+      $('.movie-info').empty();
+      let $title = $('<h3>').addClass('title').html(data.Title);
+      let $plot = $('<div>').addClass('plot').html(data.Plot);
+      let $director = $('<div>').addClass('director').html('Directed by: ' + (data.Director));
+      let $stars = $('<div>').addClass('stars').html('Cast: ' + (data.Actors));
+      $('.movie-info').append($title);
+      $('.movie-info').append($plot);
+      $('.movie-info').append($director);
+      $('.movie-info').append($stars);
+    },
+    () => {
+      console.log('bad request');
+    }
+  )
+});
+
+// event listner for 2011 button
+$('.10sbtn').on('click',(event) => {
+  movieName = 'iron+lady';
+
+// use AJAX to retrieve data from the omdb api
+  $.ajax({
+    url: "http://www.omdbapi.com/?i=tt3896198&apikey=c55d5c9f&t=" + movieName
+  }).then(
+    (data) => {
+      console.log(data);
+      $('.movie-info').empty();
+      let $title = $('<h3>').addClass('title').html(data.Title);
+      let $plot = $('<div>').addClass('plot').html(data.Plot);
+      let $director = $('<div>').addClass('director').html('Directed by: ' + (data.Director));
+      let $stars = $('<div>').addClass('stars').html('Cast: ' + (data.Actors));
+      $('.movie-info').append($title);
+      $('.movie-info').append($plot);
+      $('.movie-info').append($director);
+      $('.movie-info').append($stars);
+    },
+    () => {
+      console.log('bad request');
+    }
+  )
+});
+
+$('.right-side').on('click',() => {
+  $('.movie-info').empty();
+})
 
 })
