cfe7a8744c99473b3431f9b9fb6522b9089cb107
Lauren Mellon <laumellon@gmail.com>
2019-04-16 15:22:08 -0400

    relocated files to root directory

---
+++ app.js
@@ -0,0 +1,99 @@
+$(() => {
+/////////////////////////////////////////////////////////////////
+///////////////// create image CAROUSEL   ///////////////////////
+
+// variables need to be able to change.  have to use let instead of const
+let picCount = 0;
+let numberOfPics = $('.images').children().length - 1;
+let $visiblePic = $('.images').children();
+
+// create event listeners for BUTTONS
+
+// next BUTTON
+$('.next').on('click',() => {
+  // hide current picture
+  $visiblePic.eq(picCount).hide();
+  // to show next picture, move up to the next index
+  if (picCount < numberOfPics) {
+    picCount++;
+  } else {
+    // when you reach the end, reset to the first index
+    picCount = 0;
+  }
+  // show next picture
+  $visiblePic.eq(picCount).show();
+})
+
+// previous BUTTON
+$('.previous').on('click',() => {
+  // hide current picture
+  $visiblePic.eq(picCount).hide();
+  // to show next picture, count down to previous index
+  if (picCount > 0) {
+    picCount--;
+  } else {
+    // reset picture number to last one in array
+    picCount = numberOfPics;
+  }
+  // show the previous picture
+  $visiblePic.eq(picCount).show();
+})
+
+/////////////////////////////////////////////////////////////////
+////////////////////    create ajax method   ////////////////////
+// make ajax a global function with a parameter of the movie name we want pulled.
+
+const pullMovieData = (movieName) => {
+  $.ajax({
+    url: "http://www.omdbapi.com/?i=tt3896198&apikey=c55d5c9f&t=" + movieName
+  }).then(
+    (data) => {
+      // console.log(data);
+      // make sure no movie information is displayed
+      $('.movie-info').empty();
+      // create 4 divs for each piece of data we are pulling from the api
+      let $title = $('<h3>').addClass('title').html(data.Title);
+      let $plot = $('<div>').addClass('plot').html(data.Plot);
+      let $director = $('<div>').addClass('director').html('Directed by: ' + (data.Director));
+      let $stars = $('<div>').addClass('stars').html('Cast: ' + (data.Actors));
+      // append the divs to the last div, movie-info, so they display at the bottom of the screen
+      $('.movie-info').append($title);
+      $('.movie-info').append($plot);
+      $('.movie-info').append($director);
+      $('.movie-info').append($stars);
+    },
+    () => {
+      console.log('bad request');
+    }
+  )
+}
+
+
+//////////////////////////////////////////////////////////
+//////////////    create event listeners    //////////////
+
+// For the year buttons, pass the movie title as the parameter of pullMovieData().  This alters the ajax url everytime to display the correct info.
+
+$('.80sbtn').on('click',(event) => {
+  pullMovieData('out+of+africa');
+});
+
+$('.90sbtn').on('click',(event) => {
+  pullMovieData('The+bridges+of+madison+county');
+});
+
+$('.00sbtn').on('click',(event) => {
+  pullMovieData('The+devil+wears+Prada');
+});
+
+$('.10sbtn').on('click',(event) => {
+  pullMovieData('iron+lady');
+});
+
+// create event listner to clear the movie information when you click anywhere in the 'right-side' class. (essentially the right side of the screen)
+
+$('.right-side').on('click',() => {
+  $('.movie-info').empty();
+});
+
+});
